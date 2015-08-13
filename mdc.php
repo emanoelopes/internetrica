dad9f86a433e8c4c51d6d82fdef94a6613eeb642
Emanoel <emanoelopes@gmail.com>
2015-08-13 07:22:25 -0700

    mdc ok

--- mdc.php
+++ mdc.php
@@ -7,8 +7,24 @@
         <body>
         <h1>MDC</h1>
         <form action="mdc.php" method="post">
-                <input type="text" placeholder="digite o numero"/>
+                <input type="text" name="n1" placeholder="digite o primeiro numero"/>
+                <input type="text" name="n2" placeholder="digite o segundo numero"/>
+
                 <input type="submit" value="calcular"/>
         </form>
+<?php
+$n1 = $_POST['n1'];
+$n2 = $_POST['n2'];
+function mdc($n1, $n2){
+    $a = max($n1, $n2);
+    $b = min($n1, $n2);
+    if($a%$b == 0){
+            return $b;
+    } else {
+        return mdc($b, ($a%$b));    
+    }
+}
+echo mdc($n1,$n2);
+?>
 </body>
 </html>
