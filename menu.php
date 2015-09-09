ff23c2dcd6df9b4b0061e2817e5f8ae88c58e645
Emanoel Lopes <emanoel@virtual.ufc.br>
2015-09-09 09:31:52 -0300

    Template

--- menu.php
+++ menu.php
@@ -1,4 +1,33 @@
-
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<style>
+header {
+    background-color:black;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:100px;
+    float:left;
+    padding:5px;
+}
+section {
+    width:350px;
+    float:left;
+    padding:10px;
+}
+footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+    padding:5px;
+}
+</style>
 <?php
 
 $usuario = $_POST['usuario'];
@@ -6,13 +35,24 @@ $senha = $_POST['senha'];
 
 //Validacao
 if (($usuario == abcd) && ($senha == "123")){
+        echo "<header>";
+        echo "<h1>Internet Rica</h1>";
+        echo "</header>";
+        echo "<nav>";
         echo "<a href='fatorial.php'>Fatorial</a></br>";
         echo "<a href='mdc.php'>MDC</a></br>";
         echo "<a href='form.html'>Formulário</a></br>";
-
+        echo "</nav>";
 }else {
 
         echo '<div class="alert alert-danger" role="alert">usuario/senha incorreto. Tente novamente.</div>';
 
 }
 ?>
+<section>
+<h1>Disciplina de Internet Rica</h1>
+<p>Parte integrante do curso de ADS</p>
+</section>
+<footer>
+ADS | Prof. Paulo Vilar
+</footer>
